5806e4467d6d4af6e8010b0a2e8891cc13c57944
Vemon233 <1004381572@qq.com>
2023-04-20 00:13:56 +0800

    abc

--- First.js
+++ First.js
@@ -1,1 +1,16 @@
-console.log("First Version")
+// ==UserScript==
+// @name         Test
+// @namespace    http://www.baidu.com/
+// @version      0.2
+// @description  the wodsdf
+// @author       You
+// @match        https://github.com/login/device/success
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
